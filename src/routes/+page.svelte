562939cf0b2f65c6c6cf4e96a05ea34bbf0ab7c5
Lucas Clupny <clupnyluke@clupny.com>
2023-11-06 12:36:25 -0500

    removed code related to minesweeper functionality and cell code to its own file and added a counter as well as some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,239 +1,200 @@
 <script lang="ts">
-	let rows = 16;
-	let columns = 16;
-	let mines = 40;
+	import '@fontsource/poppins';
+	import 'material-icons/iconfont/material-icons.css';
+	import Cell from '../components/Cell.svelte';
+	import Minesweeper from '../utils/Minesweeper';
 
-	let board: number[][];
-	let playBoard: (number | string | undefined)[][];
+	const gameOngoing = 'sentiment_satisfied';
+	const gameOverIcon = 'sentiment_very_dissatisfied';
+	const gameWinIcon = 'mood';
 
-	let mineCounter = mines;
-	let spotCounter = rows * columns - mines;
-	let error: string | undefined;
-	let gameState = 0;
+	let game = new Minesweeper();
+	let seconds = 0;
+	let intervalID: NodeJS.Timeout;
 
-	function* shuffle(array: number[]) {
-		let i = array.length + 1;
-
-		while (i--) {
-			yield array.splice(Math.floor(Math.random() * i), 1)[0];
-		}
-	}
-
-	const countBombs = (row: number, column: number) => {
-		let count = 0;
-		if (board[row][column] === -1) {
-			return -1;
-		}
-
-		for (let i = -1; i <= 1; i++) {
-			for (let j = -1; j <= 1; j++) {
-				if ((board[row + i] ?? [])[column + j] === -1) count++;
-			}
-		}
-		return count;
-	};
-
-	const resetBoard = () => {
-		gameState = 0;
-		spotCounter = rows * columns - mines;
-		mineCounter = mines;
-		if (rows * columns - mines < 1) {
-			error = 'INVALID BOARD';
-			return;
-		}
-		error = '';
-		board = [];
-		playBoard = [];
-		for (let i = 0; i < rows; i++) {
-			board[i] = new Array(columns);
-			playBoard[i] = new Array(columns);
-		}
-		const possibleValues = new Array(rows * columns).fill(0).map((_, i) => i);
-		const spots = shuffle(possibleValues);
-		for (let i = 0; i < mines; i++) {
-			const spot = spots.next().value as number;
-			const row = Math.floor(spot / columns);
-			const column = spot % columns;
-			board[row][column] = -1;
-		}
-		let row = 0;
-		let column = -1;
-		for (let i = 0; i < rows * columns; i++) {
-			if (column == columns - 1) {
-				row++;
-				column = -1;
-			}
-			column++;
-			board[row][column] = countBombs(row, column);
-		}
-	};
-	resetBoard();
-
-	const revealSpot = (row: number, column: number) => {
-		const val = board[row][column];
-		if (val === -1) {
-			playBoard[row][column] = 'X';
-			gameState = -1;
-			return;
-		}
-		spotCounter--;
-		if (spotCounter == 0) {
-			gameState = 1;
-		}
-
-		playBoard[row][column] = board[row][column];
-		if (val === 0) {
-			for (let i = -1; i <= 1; i++) {
-				for (let j = -1; j <= 1; j++) {
-					if (row + i < 0 || row + i == rows) continue;
-					if (column + j < 0 || column + j == columns) continue;
-					if (playBoard[row + i][column + j] !== undefined) continue;
-					revealSpot(row + i, column + j);
-				}
-			}
-		}
-		playBoard[row][column] = board[row][column];
-	};
+	game.resetBoard();
+	game.funcsToRunOnReset.push(() => {
+		seconds = 0;
+		clearInterval(intervalID);
+	});
 
 	const clickSpot = (e: MouseEvent, row: number, column: number) => {
-		if (gameState !== 0) {
+		if (game.gameState !== 0) {
 			return;
 		}
-		if (playBoard[row][column] !== undefined && e.button == 0) {
+		if (seconds == 0) {
+			intervalID = setInterval(() => {
+				if (game.gameState === 0) {
+					seconds += 1;
+				}
+			}, 1000);
+		}
+		if (game.playBoard[row][column] !== undefined && e.button == 0) {
 			return;
 		}
 		if (e.button == 2) {
-			if (playBoard[row][column] == undefined) {
-				playBoard[row][column] = '!';
-				mineCounter--;
-				return;
-			}
-			if (playBoard[row][column] === '!') {
-				playBoard[row][column] = undefined;
-				mineCounter++;
-			}
+			game.toggleFlag(row, column);
+			game = game;
 			return;
 		}
 		if (e.button == 0) {
-			revealSpot(row, column);
+			game.revealSpot(row, column);
+			game = game;
 		}
 	};
 </script>
 
-<main>
-	<div>
-		Rows: <input type="number" bind:value={rows} min="2" on:change={() => resetBoard()} />
-		Columns: <input type="number" bind:value={columns} min="2" on:change={() => resetBoard()} />
-		Mines:<input type="number" bind:value={mines} min="1" on:change={() => resetBoard()} />
-		<input type="button" value="Reset" on:click={() => resetBoard()} />
+<main on:contextmenu|preventDefault>
+	<h1>Minesweeper</h1>
+	<div class="scoreboard">
+		<input
+			type="button"
+			value={game.gameState >= 0 ? (game.gameState > 0 ? gameWinIcon : gameOngoing) : gameOverIcon}
+			class="reset material-icons-outlined"
+			on:click={() => {
+				game.resetBoard();
+				game = game;
+			}}
+		/>
+		<div>
+			{#if game.gameState == 1}<h2>You Win in {seconds} Seconds</h2> {:else} {seconds} {/if}
+		</div>
 	</div>
-	<div>
-		{error ?? ''}
-	</div>
-	<div>
-		{#if board}
+	<div class="gameboard">
+		{#if game.board}
 			<table>
-				{#each playBoard as row, rowIndex}
+				{#each game.playBoard as row, rowIndex}
 					<tr>
 						{#each row as cell, columnIndex}
-							<!-- svelte-ignore a11y-click-events-have-key-events -->
-							<td
-								class={`cell cell-${
-									playBoard[rowIndex][columnIndex] == '!'
-										? 'flag'
-										: playBoard[rowIndex][columnIndex] ?? 'unknown'
-								}`}
-								on:mousedown={(e) => {
-									clickSpot(e, rowIndex, columnIndex);
-								}}
-								on:contextmenu|preventDefault
-							>
-								{cell ?? ' '}
-							</td>
+							<Cell {cell} {rowIndex} {columnIndex} playBoard={game.playBoard} {clickSpot} />
 						{/each}
 					</tr>
 				{/each}
 			</table>
 		{/if}
 	</div>
-	<div>
-		Mines Left: {mineCounter}
+	<div class="flagCounter">
+		<span class="material-icons">flag</span>'s to place: {game.mineCounter}
 	</div>
-	{#if gameState}
-		<h1>
-			{gameState === 1 ? 'You Win' : 'You Lose'}
-		</h1>
-	{/if}
+	<div>
+		<h2>Settings</h2>
+		<div>
+			{game.error ?? ''}
+		</div>
+		<div>
+			Presets: <select
+				value="1"
+				on:change={(e) => {
+					switch (e.currentTarget.value) {
+						case '0':
+							game.resetBoard(10, 10, 10);
+							break;
+						case '2':
+							game.resetBoard(16, 30, 99);
+							break;
+						default:
+							game.resetBoard(16, 16, 40);
+					}
+					game = game;
+				}}
+			>
+				<option value="0">Easy</option>
+				<option value="1">Medium</option>
+				<option value="2">Hard</option>
+			</select>
+		</div>
+		<div>
+			Rows:<input
+				type="number"
+				bind:value={game.rows}
+				min="2"
+				on:change={() => {
+					game.resetBoard();
+					game = game;
+				}}
+			/>
+		</div>
+		<div>
+			Columns:
+			<input
+				type="number"
+				bind:value={game.columns}
+				min="2"
+				on:change={() => {
+					game.resetBoard();
+					game = game;
+				}}
+			/>
+		</div>
+		Mines:<input
+			type="number"
+			bind:value={game.mines}
+			min="1"
+			on:change={() => {
+				game.resetBoard();
+				game = game;
+			}}
+		/>
+	</div>
 </main>
 
 <style lang="scss">
-	td {
-		border: inset 2px;
-		width: 30px;
-		height: 30px;
+	:global(body) {
+		margin: 0;
+	}
+	main {
+		min-height: 100vh;
+		font-family: 'Poppins', sans-serif;
+		background: rgb(125, 169, 252);
+		background: linear-gradient(343deg, rgba(125, 169, 252, 1) 0%, rgba(83, 204, 245, 1) 100%);
+	}
+
+	h1 {
 		text-align: center;
+		margin: 0 0 5px 0;
+	}
+	.scoreboard {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-size: 24px;
+
+		& > {
+			width: max-content;
+		}
+
+		input {
+			font-size: 32px;
+			background-color: yellow;
+			border-radius: 5px;
+			border: outset;
+			cursor: pointer;
+
+			&:hover {
+				border: inset;
+			}
+		}
 	}
 
-	.cell-X {
-		border: outset 3px red;
-		background-color: red;
+	.gameboard {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		table {
+			background: black;
+		}
 	}
 
-	.cell-unknown {
-		border: outset 3px gray;
-		background: gray;
-	}
+	.flagCounter {
+		margin: 15px 0;
+		display: flex;
+		justify-content: center;
+		font-size: 24px;
+		align-items: center;
 
-	.cell-flag {
-		border: outset 3px yellow;
-		background-color: yellow;
-	}
-
-	.cell-1 {
-		border: outset 3px #07119c;
-		background-color: #07119c;
-		color: #fff;
-	}
-
-	.cell-2 {
-		border: outset 3px #125f03;
-		background-color: #125f03;
-		color: #fff;
-	}
-
-	.cell-3 {
-		border: outset 3px #c27b11;
-		background-color: #c27b11;
-		color: #fff;
-	}
-
-	.cell-4 {
-		border: outset 3px #9f11c2;
-		background-color: #9f11c2;
-		color: #fff;
-	}
-
-	.cell-5 {
-		border: outset 3px #068cca;
-		background-color: #068cca;
-		color: #ffffff;
-	}
-
-	.cell-6 {
-		border: outset 3px #441f05;
-		background-color: #441f05;
-		color: #ffffff;
-	}
-
-	.cell-7 {
-		border: outset 3px #350544;
-		background-color: #350544;
-		color: #ffffff;
-	}
-
-	.cell-8 {
-		border: outset 3px #ffffff;
-		background-color: #ce31b4;
-		color: #ffffff;
+		span {
+			font-size: 28px;
+		}
 	}
 </style>
